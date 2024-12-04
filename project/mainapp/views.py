c8787c3c3cbd1109c6bd6a1569a9deb1a59574bc
말랑곰 <leehoje000@gmail.com>
2024-12-04 17:45:25 +0900

    feat: LLM모델과 장고 연결
    기존의 더미답변받는 부분을 LLM답변을 받아오는 book_model.get_response함수로 교체

--- project/mainapp/views.py
+++ project/mainapp/views.py
@@ -1,6 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from model import book_model
 from django.shortcuts import render
 from django.http import JsonResponse
-from .llm import generate_response  # LLM 코드 import
 import json
 
 def home(request):
@@ -14,7 +17,7 @@ def get_response(request):
         try:
             data = json.loads(request.body)
             user_input = data.get('message', '')
-            response = generate_response(user_input)
+            response = book_model.get_response(user_input)
             return JsonResponse({'response': response})
         except json.JSONDecodeError: # 잘못된 JSON 데이터로 인한 서버 오류 방지
             return JsonResponse({'error': 'Invalid JSON'}, status=400)
